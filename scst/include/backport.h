d2142/scst

commit 84bd33f9a18c6b4780f38acc6396ad4f6f870f0a

scst_mem: Port to Linux kernel v6.0
Support for the following mm layer changes in the Linux kernel v6.0:
- e33c267ab70d ("mm: shrinkers: provide shrinkers with names")

scst/include/backport.h
@@ -773,6 +773,22 @@ static inline void kvfree(void *addr)
 }
 #endif
 
+/* <linux/shrinker.h> */
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 0, 0)
+/*
+ * See also commit e33c267ab70d ("mm: shrinkers: provide shrinkers with
+ * names") # v6.0.
+ */
+static inline
+int register_shrinker_backport(struct shrinker *shrinker, const char *fmt, ...)
+{
+	return register_shrinker(shrinker);
+}
+
+#define register_shrinker register_shrinker_backport
+#endif
+
 /* <linux/module.h> */
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 4, 0)
 #define MODULE_IMPORT_NS(ns)

scst/src/scst_mem.c
@@ -1794,7 +1794,7 @@ int scst_sgv_pools_init(unsigned long mem_hwmark, unsigned long mem_lwmark)
 	sgv_shrinker.shrink = sgv_shrink;
 #endif
 	sgv_shrinker.seeks = DEFAULT_SEEKS;
-	register_shrinker(&sgv_shrinker);
+	register_shrinker(&sgv_shrinker, "scst-sgv");
 
 out:
 	TRACE_EXIT_RES(res);

commit add441b44521bf1d397de594181123de6cfaebaa

scst/include/backport.h: Fix the register_shrinker() backport
The register_shrinker() function has begun return non void value since
v3.12 version. So fix the backport of this function for kernel versions
before v3.12.

Fixes: 84bd33f9a18c ("scst_mem: Port to Linux kernel v6.0")

scst/include/backport.h
@@ -783,7 +783,15 @@ static inline void kvfree(void *addr)
 static inline
 int register_shrinker_backport(struct shrinker *shrinker, const char *fmt, ...)
 {
+/*
+ * See also commit 1d3d4437eae1 ("vmscan: per-node deferred work") # v3.12
+ */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 12, 0)
 	return register_shrinker(shrinker);
+#else
+	register_shrinker(shrinker);
+	return 0;
+#endif
 }
 
 #define register_shrinker register_shrinker_backport